49f2c31aec762e516e254f2886f8ce2daa50bca1
Anna Luisa <annaluisasantos08@gmail.com>
2026-07-15 18:25:42 -0300

    Add login and sign-up

---
+++ src/main/java/br/com/zuppy/site/SiteZuppyApplication.java
@@ -0,0 +1,12 @@
+package br.com.zuppy.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteZuppyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiteZuppyApplication.class, args);
+    }
+}
---
+++ src/main/java/br/com/zuppy/site/dto/RegisterRequest.java
@@ -0,0 +1,25 @@
+package br.com.zuppy.site.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+        @NotBlank
+        @Size(max = 150)
+        String nome,
+
+        @NotBlank
+        @Email
+        @Size(max = 150)
+        String email,
+
+        @NotBlank
+        @Size(min = 8, max = 100)
+        String senha,
+
+        @NotBlank
+        @Size(min = 11, max = 18)
+        String cpfCnpj
+) {
+}
